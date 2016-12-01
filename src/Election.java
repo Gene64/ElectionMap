2180478c3fad3ba17f180f86333b123d7cb2c7ab
Iosnowore <genepressinger@yahoo.com>
2016-12-01 17:33:01 -0500

    Clean up

--- src/Election.java
+++ src/Election.java
@@ -19,31 +19,23 @@ public class Election {
     StdDraw.setXscale(minValues[0], maxValues[0]);
     StdDraw.setYscale(minValues[1], maxValues[1]);
     
-    //double[] xValues = {0.9, 0.5, 0.1, 0.5};
-    //double[] yValues = {0.5, 0.9, 0.5, 0.1};
-    //StdDraw.setPenColor(StdDraw.BLUE);
-    //StdDraw.filledPolygon(xValues, yValues);
-    
     int numberOfSubRegions = myScanner.nextInt();
-    System.out.println("debug");
     
     while (myScanner.hasNext())
     	drawUSA(myScanner);
   }
   
   public static void drawUSA(Scanner scanner) throws FileNotFoundException {
-	  File txt = new File("USA.txt");
-	  //Scanner myScanner = new Scanner(txt);
 	  System.out.println("debug1");
 	  String stateName = scanner.next();
+	  
 	  if (stateName.equals("District"))
-		  stateName = "Disctruct of Columbia";
+		  stateName += " of Columbia";
+	  
 	  scanner.nextLine();
-	  System.out.println("debug3: " + stateName);
 	  int republicanVotes = scanner.nextInt();
 	  int democratVotes = scanner.nextInt();
 	  int independentVotes = scanner.nextInt();
-	  System.out.println("debug2");
 	  int polygonPoints = scanner.nextInt();
 	    
 	  System.out.println(polygonPoints);
